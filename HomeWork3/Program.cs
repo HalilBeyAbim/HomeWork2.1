2dbc25124708d96f6d15bffbb0b855f90413053d
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-04-14 00:30:38 +0400

    main

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("bir soz daxil edin");
+           string names = Console.ReadLine();
+            if (names.Length >= 5) 
+            {
+                Console.WriteLine(names.Length);
+            }
+            else if (names.Length < 5)
+            {
+                Console.WriteLine("sozler 5 herfi kecmir");
+            }
+
+        }
+    }
+}
